b232e25c4cc68ca21f6dab36c756239210ca1f47
obaid147 <obaidfayazwani@gmail.com>
2022-12-04 00:28:01 +0530

    Composite Directives, Actionable Directives with failed and rejected routes, Exerices for missing tilde operator

--- src/main/scala/part3_highlevelserver/DirectivesBreakdown.scala
+++ src/main/scala/part3_highlevelserver/DirectivesBreakdown.scala
@@ -111,6 +111,116 @@ object DirectivesBreakdown extends App {
         }
       }
     }
-  Http().bindAndHandle(extractRequestRoute2, "localhost", 8080)
+  //Http().bindAndHandle(extractRequestRoute2, "localhost", 8080)
   /*SSL Error. The server certificate is invalid. Certificate has expired*/
+
+  /** Type3:- Composite Directives*/
+  val simpleNestedRoute: Route =
+    path("api" / "item") {
+      get {
+        complete(StatusCodes.OK)
+      }
+    }
+  val compactSimpleNestedRoute: Route =
+    (path("api" / "item") & get) { // combining directives using "&"
+      complete(StatusCodes.OK)
+    }
+
+  val compactExtractRequestRoute: Route = // equivalent to extractRequestRoute2
+    (path("controlEndpoint") & extractRequest & extractLog) { (httpRequest, log) =>
+      log.info(s"Got the requested path:- ${httpRequest.uri}")
+      complete(StatusCodes.OK)
+    }
+
+  //  /about or /aboutUs will result in same response
+  val repeatedRoute: Route =
+    path("about") {
+      complete(StatusCodes.OK)
+    } ~
+      path("aboutUs") {
+        complete(StatusCodes.OK)
+      }
+
+  val dryRoute: Route =
+    (path("about") | path("aboutUs")) { // OR
+      complete(StatusCodes.OK)
+    }
+
+  /*// implementing a blog to support endpoints
+  * 1. yourblog.com/20
+  * 2. yourblog.com?postId=20
+  * */
+  val blogByIdRoute =
+    path("yourblog.com" / IntNumber) { (blogId: Int) =>
+      //complex server logic here
+      println(s"extracted:- $blogId")
+      complete(StatusCodes.OK)
+    }
+  val blogByQueryParam =
+    path("yourblog.com") {
+      parameter('blogId.as[Int]) { (blogPostId: Int) =>
+        //complex server logic here
+        println(s"Parameter extracted $blogPostId")
+        complete(StatusCodes.OK)
+      }
+    }
+
+  val combinedBlogByIdRoute = {
+    // path pm and parameter type must be same and also the number of args must be same.
+    (path("yourblog.com" / IntNumber) | parameter('blogId.as[Int])) { (blogPostId: Int) =>
+      println(s"Extracted Id:- $blogPostId and ")
+      complete(StatusCodes.OK)
+    }
+  }
+
+  //  Http().bindAndHandle(combinedBlogByIdRoute, "localhost", 8080)
+
+  /** Type4:- Actionable Directives*/
+  val completeOkRoute: Route = complete(StatusCodes.OK)
+  // complete takes a function from m: ⇒ ToResponseMarshallable
+  // (ToResponseMarshallable) means anything that can be converted to HttpResponse usually via implicit conversion.
+
+  val failedRoute =
+    path("notSupported") {
+      failWith(new RuntimeException("UnSupported")) // 500 Internal server error.
+    }
+
+  /*REJECTING A request means handing it over to the next possible handler in the routing tree.*/
+  val routeWithRejection =
+    path("home") { /*If we want to reject a route, we should not write the route In the first place*/
+      reject
+    } ~
+      path("index") {
+        complete(HttpEntity( // or we can reuse completeOkRoute
+          ContentTypes.`text/html(UTF-8)`,
+          """
+            |<html>
+            |<body>
+            |Hi There
+            |</body>
+            |</html>
+            |""".stripMargin
+        ))
+      }
+
+  /** EXERCISE spot the mistake*/
+  val getOrPutPath =
+   /* path("api" / "myEndpoint") {
+      get {
+        completeOkRoute
+      }
+      post {
+        complete(StatusCodes.Forbidden)
+      }
+    }*/
+    path("api" / "myEndpoint") {
+      get {
+        completeOkRoute
+      } ~// missing tilde operator
+      post {
+        complete(StatusCodes.Forbidden)
+      }
+    }
+  Http().bindAndHandle(getOrPutPath, "localhost", 8080)
+
 }
